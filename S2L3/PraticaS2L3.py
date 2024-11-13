9b00fc6615a8b9ea7b126e471a56a46f180d869f
Alex Dorz <alex.dorz.epicode@gmail.com>
2024-11-13 10:45:18 -0500

    13-11-2024

---
+++ S2L3/PraticaS2L3.py
@@ -0,0 +1,4 @@
+print("Troviamo il nome della tua Band!")
+citta = input("Indica una città: ")
+nome = input("Inserisci il nome del tuo animale domestico: ").upper()
+print(f"Da oggi sarai {nome}!! il miglior menestrello di {citta}")
